91044f337df49d8051f61852de61c98e4ed43257
WyattKepo10 <wyattngo986@gmail.com>
2024-09-26 14:47:43 -0500

    Totally didnt base this off of star wars...

---
+++ Assignments/MadLib.py
@@ -0,0 +1,20 @@
+    # In a galaxy far far away
+    # A [gender] lived in a small hut in the middle of a [region]
+    # However [pronoun] was approached by a [gender2] who was named [name]
+    # [pronoun2] wore a cloak and taught the loenly person [martial art]
+    # The [gender] would then go to a [place] and fight the villian that lived there
+    # However, the villian revealed to be their [family member] to clarify, like, mom, dad, uncle, ect.
+    # The person was devastated and felt [emotion]
+    # The person beat the villian down and showed them [noun]
+gender = input("insert gender\n>")
+pronoun = input("What is the gender pronoun above?\n>")     #he/she/they
+region = input("Insert a region\n>")
+gender2 = input("Insert another gender\n>")
+pronoun2 = input("What is the gender pronoun above?\n>")    #he/she/they
+name = input("What is the person above's name?\n>")
+ma=input("Name a martial art\n>")
+place=input("Name a building type\n>") # tower, castle, deathstar, ect.
+emotion=input("Name an emotion\n>")
+noun=input("Name a noun\n>")
+fm=input("Name a family member role\n>")    # mom, dad, uncle, ect.
+print("In a galaxy far far away, a " + gender + " lived in a small hut in the middle of a " + region + ". After some time passed, " + pronoun + " was approached by a " + gender2 + " in a cloak. " + pronoun2 + " was named "+ name + "." + gender2 + " taught the lonely " + gender + "a martial art named " + ma + ". The " + gender + " would then head to a " + place + " and fight the villian that settled there. However, the villian would then revealed themslves to be the " + gender + "s " + fm + ". The " + gender + " felt " + emotion + " and showed the same blooded villian " + noun + ".")
