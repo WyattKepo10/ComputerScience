87086e366963013d5241a689a6b8ee1dd070479c
WyattKepo10 <wyattngo986@gmail.com>
2024-09-27 15:17:00 -0500

    4.5/5 understanding

---
+++ Unit 2 - Functions and Variables/Functions.py
@@ -0,0 +1,16 @@
+x = 5
+
+def my_function():      #Define the function with a name
+    print("Hello World!")
+    print("CatDog")
+
+print("Osowski r00lz d00d")
+my_function()
+
+def add(x, y):
+    print(x + y)
+
+add(15, 20)
+add(2, 2)
+add(15, 15)
+add("cat", "dog")
---
+++ Unit 2 - Functions and Variables/TopFiveMovies.py
@@ -0,0 +1,9 @@
+def movies(a, b ,c, d ,e):
+    print("Wyatt's top 5 movies!")
+    print("1." + a)
+    print("2." + b)
+    print("3." + c)
+    print("4." + d)
+    print("5. " + e)
+
+movies("Mobile Suit Gundam", "Pacific Rim", "Inside Out", "Dragon Ball Super: Broly", "Dragon Ball Super: Heroes")
