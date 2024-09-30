5ed9f187562d18e01570b8bf8d4bafed1c2b8536
WyattKepo10 <wyattngo986@gmail.com>
2024-09-30 15:04:26 -0500

    My brain is FRIED

---
+++ Assignments/Simple_Calculator.py
@@ -0,0 +1,18 @@
+def add(x, y):
+    print(x + y)
+
+def sub(a, b):
+    print(a - b)
+
+def div(m, o):
+    print(m/o)
+def mult(w, z):
+    print(w * z)
+def exp(e, r):
+    print(e + r*r)
+
+add(10, 4)
+sub(1, 5)
+div(6, 3)
+mult(5, 5)
+exp(9, 3)
