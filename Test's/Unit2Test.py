660998fcbbe20160a9bcaded94c6b167f97c5f48
WyattKepo10 <wyattngo986@gmail.com>
2024-10-03 14:45:46 -0500

    Study more in the future.

---
+++ Test's/Unit2Test.py
@@ -0,0 +1,22 @@
+#1
+print("I would like to ask for three random words from you!")
+word1 = input("Please put a random word here\n>")
+word2 = input("Please put another random word here\n>")
+word3 = input("Put the final random word here\n>")
+print("Thank you for the random words! The words you have inputed was: " + word1 + ", " + word2 + ", and " + word3 + ".")
+print("May you give me more words, again!")
+
+#2
+def add_three (a, b, c):    
+    print( a + b + c )
+add_three (1, 2, 4)
+
+#3
+def data_three():
+    word = input("Enter a word, here\n>")
+    interger = input("Enter a interger, here\n>")
+    float = input("Enter a float, here\n>")
+    print( float + interger + word )
+
+data_three()
+#3 had me stumped, i apologize.
